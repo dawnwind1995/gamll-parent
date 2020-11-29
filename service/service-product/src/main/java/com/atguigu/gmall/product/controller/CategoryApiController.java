edc9924437b52443a263556dbca0d4574c676061
晨曦风起 <745176936@qq.com>
2020-11-29 16:56:39 +0800

    正常

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryApiController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryApiController.java
@@ -4,12 +4,11 @@ package com.atguigu.gmall.product.controller;
 import com.atguigu.gmall.common.Result.Result;
 import com.atguigu.gmall.model.product.BaseCategory1;
 
-import com.atguigu.gmall.product.service.BaseCategory1Service;
+import com.atguigu.gmall.model.product.BaseCategory2;
+import com.atguigu.gmall.model.product.BaseCategory3;
+import com.atguigu.gmall.product.service.BaseCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +19,26 @@ import java.util.List;
 @RequestMapping("admin/product")
 @RestController
 @CrossOrigin
-public class CategoryApiController {
+public class CategoryApiController
+{
     @Autowired
-    private BaseCategory1Service baseCategory1Service;
+    private BaseCategoryService baseCategoryService;
+    //一级分类
     @GetMapping("getCategory1")
     public Result getCategory1(){
-        List<BaseCategory1> baseCategory1List =baseCategory1Service.getCategory1();
+        List<BaseCategory1> baseCategory1List =baseCategoryService.getCategory1();
         return Result.ok(baseCategory1List);
+    }
+    //二级分类
+    @GetMapping("getCategory2/{category1Id}")
+    public Result getCategory2(@PathVariable("category1Id") Long  category1Id){
+        List<BaseCategory2> baseCategory2List =baseCategoryService.getCategory2(category1Id);
+        return Result.ok(baseCategory2List);
+    }
+    //三级分类
+    @GetMapping("getCategory3/{category2Id}")
+    public Result getCategory3(@PathVariable("category2Id") Long  category2Id){
+        List<BaseCategory3> baseCategory3List =baseCategoryService.getCategory3(category2Id);
+        return Result.ok(baseCategory3List);
     }
 }
